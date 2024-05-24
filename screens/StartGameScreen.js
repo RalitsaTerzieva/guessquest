2576655e0abbc70c8799b1678d98f9f38d0efde1
Ralica Popova <ralicaterzieva90@gmail.com>
2024-05-24 17:50:43 +0300

    Add screens logic

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,10 @@
+import { TextInputv, View } from 'react-native';
+
+function StartGameScreen() {
+    return <View>
+        <TextInput />
+        
+    </View>
+}
+
+export default StartGameScreen;
