e29f8510babd848d647983d566c876ed6280e39a
Ralica Popova <ralicaterzieva90@gmail.com>
2024-05-26 17:32:03 +0300

    Add background image

--- app/index.js
+++ app/index.js
@@ -1,23 +1,27 @@
-
-
-import { StyleSheet  } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from '../screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-
   return (
-    <>
-        <LinearGradient colors={['#4e0329', '#ddb52f' ]} style={styles.rootScreen}>
-            <StartGameScreen/>
-        </LinearGradient>
-    </>
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground 
+        source={require('./../assets/images/background.png')} 
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen/>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    backgroundColor: '#ddb52f',
     flex: 1
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
