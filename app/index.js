93f27e6edd6626acb14eb8a456505f33b41e9e05
Ralica Popova <ralicaterzieva90@gmail.com>
2024-05-31 12:28:39 +0300

    Add SafeAreaView

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
 import StartGameScreen from '../screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 import GameScreen from '../screens/GameScreen';
@@ -26,7 +26,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-       {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -20,6 +20,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 12
+        padding: 24
     }
 })
