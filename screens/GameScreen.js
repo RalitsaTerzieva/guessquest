1b383c358dcdc44ab4a8110de17369c5368e2b88
Ralica Popova <ralicaterzieva90@gmail.com>
2024-06-02 12:11:13 +0300

    Add more logic to the GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,25 @@
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 function GameScreen() {
-    return <Text>Game Screen</Text>
+    return <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            Guess
+            <View>
+                <Text>Higer ot lower?</Text>
+                +
+                -
+            </View>
+            <View>
+                Log Rounds
+            </View>
+        </View>
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12
+    }
+})
