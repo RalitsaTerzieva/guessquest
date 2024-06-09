c44e7c64e0dd60aee323f45cf10d3297de7c6f69
Ralica Popova <ralicaterzieva90@gmail.com>
2024-06-09 15:06:22 +0300

    Improve Game Over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,37 @@
-import { Text } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
+import Title from '../components/ui/Title';
+import { Colors } from '../constants/Colors';
 
 function GameOverScreen() {
-    return <Text>Game is over!</Text>
+    return <View style={styles.rootContainer}>
+        <Title>GAME OVER!</Title>
+        <View style={styles.imageContainer}>
+            <Image source={require('../assets/images/success.png')} style={styles.image}/>
+        </View>
+        <Text>Your phone needed X rounds to guess the number Y</Text>
+    </View>
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        width: 300,
+        height: 300,
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+})
